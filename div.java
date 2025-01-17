da0deb1b39881641905c778f1f632757ce39f22a
Harsh Tomar <harsh.thakur191003@gmail.com>
2025-01-17 22:34:28 +0530

    Add Division Program

---
+++ div.java
@@ -0,0 +1,10 @@
+package Calculator_BL;
+import java.util.Scanner;
+public class div {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        System.out.println("Division: "+ (a/b));
+    }
+}
